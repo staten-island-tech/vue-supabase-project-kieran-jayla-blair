ea6db615918a02cf0548cc9ced44246ae4a87fc6
jaylaye <yejayla69@gmail.com>
2024-04-12 09:26:44 -0400

    .

--- social-project/src/views/HomeView.vue
+++ social-project/src/views/HomeView.vue
@@ -3,7 +3,20 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+ <div>
+  <div>
+    <form name="" @submit.prevent="login(username, password)">
+    <div>
+      <label for="username">Username: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">Password: </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    </form>
+  </div>
+ </div>
 </template>
+
+<!-- http://localhost:3000 -->
