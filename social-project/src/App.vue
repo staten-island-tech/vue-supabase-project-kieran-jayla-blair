202c77d571e6213df6faee6aa9c97727ac72e5af
kieranwatt <kieranmwatt@gmail.com>
2024-04-16 09:19:37 -0400

    bruh

--- social-project/src/App.vue
+++ social-project/src/App.vue
@@ -4,20 +4,22 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+ <div>
+  <h1>Sign In</h1>
+  <div>
+    <form name="" @submit.prevent="login(username, password)">
+    <div>
+      <label for="username">Username: </label>
+      <input id="username" type="text" v-model="username" />
     </div>
-  </header>
-
-  <RouterView />
+    <div>
+      <label for="password">Password: </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    </form>
+  </div>
+  <button>Enter</button>
+ </div>
 </template>
 
 <style scoped>
--- social-project/src/views/HomeView.vue
+++ social-project/src/views/HomeView.vue
@@ -3,20 +3,11 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
- <div>
-  <div>
-    <form name="" @submit.prevent="login(username, password)">
-    <div>
-      <label for="username">Username: </label>
-      <input id="username" type="text" v-model="username" />
-    </div>
-    <div>
-      <label for="password">Password: </label>
-      <input id="password" type="password" v-model="password" />
-    </div>
-    </form>
-  </div>
- </div>
+<div>
+  <h1>HOME</h1>
+  <h2>i</h2>
+</div>
 </template>
 
 <!-- http://localhost:3000 -->
+ 
