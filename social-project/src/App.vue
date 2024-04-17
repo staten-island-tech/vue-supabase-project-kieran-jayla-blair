4c2f28fc58c845287e5d6e390ea73d26cc69aa6f
kieranwatt <kieranmwatt@gmail.com>
2024-04-17 09:16:38 -0400

    2786138943 9832

--- social-project/src/App.vue
+++ social-project/src/App.vue
@@ -21,6 +21,31 @@ import HelloWorld from './components/HelloWorld.vue'
   <button>Enter</button>
  </div>
 </template>
+<!-- <script setup>
+import { onMounted, ref } from 'vue'
+import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
+import { supabase } from './supabase'
+
+const session = ref()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
+</script>
+
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
+  </div>
+</template> -->
 
 <style scoped>
 header {
