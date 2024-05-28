fd400880082735b9d462c3720c09f627b94e9fc0
Ben Zanelli <BenjaminZanelli51@gmail.com>
2024-05-28 16:17:50 -0400

    .

--- social-project/src/views/signIn.vue
+++ social-project/src/views/signIn.vue
@@ -1,17 +1,18 @@
 <template>
     <div>
         <form action="" >
-    <button @click.prevent="signIn">sign up</button>
+    <button @click.prevent="signIn"><RouterLink to="/homepage">Sign in</RouterLink></button>
     <input type="" v-model="email">
   <input type="password" v-model="password">
 </form>
     </div>
+<RouterView/>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import { supabase } from '@/lib/supabase';
-import
+import { RouterLink, RouterView } from 'vue-router';
 async function signIn(){
   const { data, error } = await supabase.auth.signInWithPassword({
     email: email.value,
