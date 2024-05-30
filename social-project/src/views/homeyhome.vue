9402345ef728411068c9c49c5eece1a1278d1636
kieranwatt <kieranmwatt@gmail.com>
2024-05-30 09:24:00 -0400

    bruh

--- social-project/src/views/homeyhome.vue
+++ social-project/src/views/homeyhome.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        
+         
     </div>
 </template>
 
 <script setup>
+// const { data, error } = await supabase
+//   .from('we put the name of the user folder here')
+//   .select()
+//   .is('name', null) 
 
+
+
+
+//code below might do something
+
+// create table public.profiles (
+//   id uuid not null references auth.users on delete cascade,
+//   first_name text,
+//   last_name text,
+
+//   primary key (id)
+// );
+
+// alter table public.profiles enable row level security; 
 </script>
 
 <style lang="scss" scoped>
